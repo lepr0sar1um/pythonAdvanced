eabc5a2ffee2b25587bad66474c17461685bd58d
lepr0sar1um <bw0711@gmail.com>
2020-12-28 22:07:15 +0200

    homework

--- main/main.py
+++ main/main.py
@@ -10,18 +10,21 @@ if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
+    assert parse('http://example.com') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
     assert parse('http://example.com/?color=red') == {'color': 'red'}
 
 
 def parse_cookie(query: str) -> dict:
-    result =  {_.split('=')[0]: "=".join(_.split("=")[1:]) for _ in query.split(';') if _}
-    return result
+    return {_.split('=')[0]: "=".join(_.split("=")[1:]) for _ in query.split(';') if _}
 
 
 if __name__ == '__main__':
     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
     assert parse_cookie('') == {}
+    assert parse_cookie('name=') == {'name': ''}
+    assert parse_cookie('name=Dima;age=') == {'name': 'Dima', 'age': ''}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima;age=;job=dev') == {'name': 'Dima', 'age': '', 'job': 'dev'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;job=dev') == {'name': 'Dima=User', 'age': '28', 'job': 'dev'}
