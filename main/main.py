3fcd77ecada5767516990ec89e568c19c597edfb
lepr0sar1um <bw0711@gmail.com>
2020-12-28 21:55:57 +0200

    homework

--- main/main.py
+++ main/main.py
@@ -1,5 +1,8 @@
 def parse(query: str) -> dict:
-    return {}
+    if '?' not in query:
+        return {}
+    else:
+        return {kv.split("=")[0]: kv.split("=")[1] for kv in query.split('?')[1].split('&') if kv}
 
 
 if __name__ == '__main__':
@@ -8,10 +11,12 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?color=red') == {'color': 'red'}
 
 
 def parse_cookie(query: str) -> dict:
-    return {}
+    result =  {_.split('=')[0]: "=".join(_.split("=")[1:]) for _ in query.split(';') if _}
+    return result
 
 
 if __name__ == '__main__':
@@ -19,3 +24,4 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;job=dev') == {'name': 'Dima=User', 'age': '28', 'job': 'dev'}
